b853b5d8e2aa8d135df0f363bc727313efb83bb2
AndreyzZz <Andrey.zZz@gmail.com>
2012-09-29 21:17:51 +0300

    Update translation file

--- src/RuSceneryInstaller_ru.ts
+++ src/RuSceneryInstaller_ru.ts
@@ -10,85 +10,113 @@
         <translation>Установка библиотеки RuScenery</translation>
     </message>
     <message>
-        <location filename="widget.ui" line="63"/>
+        <location filename="widget.ui" line="59"/>
         <source>X-Plane folder:</source>
         <translation>Директория X-Plane:</translation>
     </message>
     <message>
-        <location filename="widget.ui" line="97"/>
+        <location filename="widget.ui" line="93"/>
         <source>...</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="widget.ui" line="118"/>
-        <source>Download progress:</source>
-        <translation>Состояние установки:</translation>
+        <location filename="widget.ui" line="114"/>
+        <source>Select X-Plane folder and press Install button</source>
+        <translation>Выберите директорию X-Plane и нажмите кнопку Установить</translation>
     </message>
     <message>
-        <location filename="widget.ui" line="165"/>
-        <location filename="widget.cpp" line="65"/>
+        <location filename="widget.ui" line="148"/>
+        <source>%p%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="201"/>
+        <location filename="widget.cpp" line="79"/>
+        <source>New Installer available</source>
+        <translation>Доступно обновление</translation>
+    </message>
+    <message>
+        <location filename="widget.ui" line="164"/>
+        <location filename="widget.cpp" line="69"/>
         <source>Install</source>
         <translation>Установить</translation>
     </message>
     <message>
-        <location filename="widget.ui" line="180"/>
+        <location filename="widget.ui" line="185"/>
         <source>Welcome to RuScenery Installer</source>
         <translation>Вас приветствует установщик RuScenery</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="32"/>
-        <location filename="widget.cpp" line="33"/>
+        <location filename="widget.cpp" line="38"/>
+        <location filename="widget.cpp" line="172"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="65"/>
+        <location filename="widget.cpp" line="69"/>
         <source>Abort</source>
         <translation>Отмена</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="73"/>
+        <location filename="widget.cpp" line="91"/>
         <source>Select X-Plane directory</source>
         <translation>Директория X-Plane</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="108"/>
+        <location filename="widget.cpp" line="131"/>
         <source>Installation started</source>
         <translation>Начало установки</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="128"/>
+        <location filename="widget.cpp" line="152"/>
         <source>Downloading %1</source>
         <translation>Скачивается %1</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="169"/>
-        <source>Unable to download version file. File too big.</source>
-        <translation>Ошибка закачки файла. Очень большой файл.</translation>
+        <location filename="widget.cpp" line="176"/>
+        <source>Aborted by user</source>
+        <translation>Прервано пользователем</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="239"/>
+        <location filename="widget.cpp" line="177"/>
+        <source>Installation finished</source>
+        <translation>Установка завершена</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="215"/>
+        <location filename="widget.cpp" line="342"/>
+        <location filename="widget.cpp" line="356"/>
+        <source>Invalid file size %1 url:%2</source>
+        <translation>Неверный размер файла %1 url:%2</translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="265"/>
+        <location filename="widget.cpp" line="351"/>
+        <source>%1 url:%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="widget.cpp" line="285"/>
         <source>It&apos;s need to download %1 MiB of files.
  Do you want to continue?</source>
         <translation>Необходимо скачать %1 MiB
 Хотите продолжить?</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="262"/>
+        <location filename="widget.cpp" line="302"/>
         <source>Downloading %1 - %2 MiB</source>
         <translation>Скачивается %1 - %2 MiB</translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="343"/>
-        <source>All files downloaded</source>
-        <translation>Все файлы установлены</translation>
+        <location filename="widget.cpp" line="310"/>
+        <location filename="widget.cpp" line="333"/>
+        <source>%1 fileName:%2</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="widget.cpp" line="350"/>
-        <source>New %1 version availables.
- Do you want to download?</source>
-        <translation>Доступно обновление программы-установщика %1.
-Вы хотите установить?</translation>
+        <location filename="widget.cpp" line="374"/>
+        <source>All files downloaded</source>
+        <translation>Все файлы установлены</translation>
     </message>
 </context>
 </TS>
